5ca841d311717a9c554bc28b160d7e8f2c74a4b4
Akihiro Ito <akihihro@yoursweetest.net>
2024-04-25 16:04:47 +0900

    JUnitを導入する
    JDKは18に戻す

--- app/src/test/java/othello/AppTest.java
+++ app/src/test/java/othello/AppTest.java
@@ -3,12 +3,16 @@
  */
 package othello;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
 }
